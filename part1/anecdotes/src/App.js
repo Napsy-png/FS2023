bdb212a0769e6f0fc838e2239fcfb6b0b6aaf3bd
Daniel Buselli <buselli.daniel@gmail.com>
2023-03-11 20:34:07 +0200

    part 1 done

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,56 @@
+
+import { useState } from 'react'
+
+const MostVotes = ({points, quotes}) => {
+  let votes = [...points]
+  let n = 0
+  for(let i=0; i < votes.length; i++) {
+    if (votes[i] > n) n = i
+  }
+ 
+  return(
+    <div>
+    <>{quotes[n]}</>
+    <br/>
+    <>has {votes[n]} votes</>
+    </div>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(Array(8).fill(0))
+
+  const handleNext = () => { 
+    setSelected(Math.floor(Math.random() * 8)) 
+  }
+  const handleVote = () => {
+    let copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={handleVote}>vote</button>
+      <button onClick={handleNext}>next anecdote</button>
+      <h1>Anecdote with the most votes</h1>
+      <MostVotes points={points} quotes={anecdotes}/>    
+    </div>
+  )
+}
+
+export default App
--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const Button = ({handleClick, text}) =>  <button onClick={handleClick}>{text}</button>
+
+const StatisticsLine = ({text, val}) => <tr><td>{text}</td><td>{val}</td></tr>
+
+
+const Statistics = ({good, neutral, bad}) => {
+
+  return (
+    <table>
+      <tbody>     
+        <StatisticsLine text="good" val={good}/>
+        <StatisticsLine text="neutral" val={neutral}/>
+        <StatisticsLine text="bad" val={bad}/>
+        <StatisticsLine text="all"val={good+neutral+bad}/>
+        <StatisticsLine text="average" val={((good - bad) / (good + neutral + bad))}/>
+        <StatisticsLine positive text="positive%"val={(good / (good + neutral + bad))*100}/>
+      </tbody>
+    </table>
+  )
+}
+
 
 function App() {
+
+const [good, setGood] = useState(0)
+const [bad, setBad] = useState(0)
+const [neutral, setNeutral] = useState(0)
+
+const handleGood = () => {
+  setGood(good + 1)
+}
+
+const handleBad = () => {
+  setBad(bad + 1)
+}
+
+const handleNeutral = () => {
+  setNeutral(neutral + 1)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>give feedback</h1>
+     <Button handleClick={handleGood} text="good"></Button>
+     <Button handleClick={handleNeutral} text="neutral"></Button>
+     <Button handleClick={handleBad} text="bad"></Button>
+     <h1>statistics</h1>
+     {good|bad|neutral? <Statistics good={good} neutral={neutral} bad={bad}/>: <p>No stats</p>}
     </div>
   );
 }
